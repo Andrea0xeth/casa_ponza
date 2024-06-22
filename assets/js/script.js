793963c694e0390287bcd63ca69ba27f5379c400
andrea0xeth <andrearitondale94@gmail.com>
2024-06-22 19:40:24 +0200

    modifica script video

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const openModalBtn = document.getElementById('open-modal-btn');
     const closeModalBtn = document.getElementById('close-modal-btn');
     const modal = document.getElementById('more-info-modal');
+    const video = document.getElementById('bg-video');
 
     openModalBtn.addEventListener('click', function () {
         modal.style.display = 'flex';
@@ -16,4 +17,8 @@ document.addEventListener('DOMContentLoaded', function () {
             modal.style.display = 'none';
         }
     });
+
+    // Forzare l'autoplay del video su dispositivi mobili
+    video.muted = true;
+    video.play();
 });
